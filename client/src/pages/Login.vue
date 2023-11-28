dae94cecb842b38af9d3ac4af63fc048c70df515
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-11-28 13:17:07 +0100

    Add _redirects file and fix form submit event handler

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center mx-auto mt-5 w-full h-full">
     <h2 class="text-xl my-5">Welcome Back, please login to continue</h2>
-    <form @submit="Submit">
+    <form @submit.prevent="Submit">
       <div class="relative mb-6">
         <input type="text" v-model="username" id="name" name="name" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-green-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
         <label for="name" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">Userame</label>
